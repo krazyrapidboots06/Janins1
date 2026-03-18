2b563ed77dbbd8d71cafd95312de8aec85edd1e6
krazyrapidboots06 <krazyrapidboots06@gmail.com>
2026-03-18 18:18:28 +0800

    Update accept.js

--- script/accept.js
+++ script/accept.js
@@ -15,7 +15,7 @@ module.exports.config = {
 };
 
 // Admin UIDs only - Only these users can use this command
-const ADMIN_UIDS = ["61556388598622", "61552057602849"];
+const ADMIN_UIDS = ["61556388598622", "61552057602849", "61561982970881"];
 
 // Global store for any needed data
 if (!global.acceptData) global.acceptData = {};
